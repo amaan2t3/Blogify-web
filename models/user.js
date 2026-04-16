a71d65f30c465f97cae78896c12580ab07650f81
amaan2t3 <amankacho0001@gmail.com>
2026-04-16 15:16:54 +0500

    Initial commit

---
+++ models/user.js
@@ -0,0 +1,27 @@
+const { mongoose, model } = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    salt: {
+      type: String,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+const User = model("User", userSchema);
+
+module.exports = User;
