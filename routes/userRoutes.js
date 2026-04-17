b4b95b734c6d758523b6233d0e45951728676be6
amaan2t3 <amankacho0001@gmail.com>
2026-04-17 17:56:42 +0500

    Implement user routes to fetch all users from the database

---
+++ routes/userRoutes.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const express = require('express');
+
+
+const router = express.Router();
+
+const User = require('../models/userModel');
+
+// Get all users
+router.get("/", async (req ,res)=>{
+    try {
+        const users = await User.find();
+        res.json(users);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
