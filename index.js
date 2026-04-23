eea0cc952e9d521eb0c041dcc76ba43c171b327b
amaan2t3 <amankacho0001@gmail.com>
2026-04-23 17:18:55 +0500

    Add user routes and update server configuration

--- index.js
+++ index.js
@@ -2,15 +2,22 @@ const path = require('path')
 const express = require('express');
 
 const app = express();
-
 const PORT = 8000;
 
+const userRoutes = require("./routes/userRoutes");
+
+
+// Middleware to parse JSON bodies
 app.set("view engine", "ejs");
 app.set("views",path.resolve("./views")); 
+
+// // Import user routes
 
 app.get("/", (req, res) => {
     res.render("home");
 });
+app.use("/users", userRoutes)
+
 
 
 app.listen(PORT, () => {
