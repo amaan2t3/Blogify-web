eb52ede565a292ed715981e83b6fb7190c98e301
amaan2t3 <amankacho0001@gmail.com>
2026-07-05 23:58:30 +0500

    modify ui

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,26 +1,26 @@
- const {express , Router }= require('express');
+const { express, Router } = require('express');
 const User = require('../models/userModels');
 
 
 const router = Router();
 
- 
+
 // Get all users
 
-router.get("/signin" , (req , res)=>{
+router.get("/signin", (req, res) => {
     return res.render("signin");
 
 });
 
- 
 
-router.get("/signup" , (req , res)=>{
+
+router.get("/signup", (req, res) => {
     return res.render("signup");
 });
- 
 
-router.post("/signup" , async (req , res)=>{
-    const {fullName , email , password} = req.body;
+
+router.post("/signup", async (req, res) => {
+    const { fullName, email, password } = req.body;
 
     try {
         const user = await User.create({
@@ -34,42 +34,42 @@ router.post("/signup" , async (req , res)=>{
         req.session.user = user;
 
         return res.redirect("/");
-    } catch(err) {
+    } catch (err) {
         console.log(err);
         res.render("signup", { error: err.message });
     }
 });
 
-router.post("/signin" , async (req , res)=>{
-    const {email , password} = req.body;
+router.post("/signin", async (req, res) => {
+    const { email, password } = req.body;
 
     try {
-        const user = await User.findOne({email});
-        
-        if(!user) {
+        const user = await User.findOne({ email });
+
+        if (!user) {
             return res.render("signin", { error: "Invalid email or password" });
         }
 
         // Verify password
         const userPassword = user.password;
-        const {createHmac} = require("crypto");
+        const { createHmac } = require("crypto");
         const hashedPassword = createHmac("sha256", user.salt)
             .update(password)
             .digest("hex");
 
-        if(hashedPassword !== userPassword) {
+        if (hashedPassword !== userPassword) {
             return res.render("signin", { error: "Invalid email or password" });
         }
 
         // Set session
-         
-         
+
+
 
         return res.redirect("/");
-    } catch(err) {
+    } catch (err) {
         console.log(err);
         res.render("signin", { error: err.message });
     }
 });
 
- module.exports = router;
+module.exports = router;
